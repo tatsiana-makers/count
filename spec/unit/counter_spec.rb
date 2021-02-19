59387dc271f78e2e2bb69b867e4c8e7aed843624
Hannah Desmond <desmond.hannah@googlemail.com>
2021-02-19 11:30:25 +0000

    Refactor counter_spec

--- spec/unit/counter_spec.rb
+++ spec/unit/counter_spec.rb
@@ -1,44 +1,35 @@
 require 'counter'
 
 describe Counter do
-  describe '#new' do
-    it 'starts with a count of 0' do
-      counter = Counter.new
-      expect(counter.count).to eq 0
-    end
-  end
+  before (:each) { @counter = Counter.new }
 
   describe '#increment' do
     it 'adds 1 to the count' do
-      counter = Counter.new
-      counter.increment
-      expect(counter.count).to eq 1
+      @counter.increment
+      expect(@counter.count).to eq 1
     end
   end
 
   describe '#decrement' do
     it 'takes 1 from the count' do
-      counter = Counter.new
-      counter.decrement
-      expect(counter.count).to eq -1
+      @counter.decrement
+      expect(@counter.count).to eq -1
     end
   end
 
   describe '#reset' do
     it 'returns the count to 0' do
-      counter = Counter.new
-      counter.increment
-      counter.reset
-      expect(counter.count).to eq 0
+      @counter.increment
+      @counter.reset
+      expect(@counter.count).to eq 0
     end
   end
 
   describe '#time' do
     it 'adds the time of last count' do
-      counter = Counter.new
-      counter.increment
-      time = counter.time
-      expect(counter.time).to eq time
+      @counter.increment
+      time = @counter.time
+      expect(@counter.time).to eq time
     end
 
   end
